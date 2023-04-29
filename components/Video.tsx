b40008b2ed96566240eec93fba2be276f62bbf40
Samuel Parker <samuel.a.parker00@gmail.com>
2023-04-29 18:57:35 -0400

    add video-component

---
+++ components/Video.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from "react";
+
+interface Props {
+  src: string;
+}
+
+const Video: FC<Props> = ({ src }) => {
+  console.log("SRC: ", src);
+  return (
+    <div className="relative pb-9/16">
+      <video className="" controls>
+        <source src={src} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+};
+
+export default Video;
