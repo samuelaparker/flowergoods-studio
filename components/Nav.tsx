a133f381126606972f988b7193503be0cc7daae3
Samuel Parker <samuel.a.parker00@gmail.com>
2023-05-04 18:42:05 -0400

    Add links

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,23 +4,37 @@ import Link from "next/link";
 interface Props {}
 
 const SidebarNav: React.FC<Props> = () => {
+  const subject = "Inquiry from flowergoods.studio";
+  const body =
+    "Hi,\n\nI wanted to get in touch with you about...\n\nBest regards,\nYour Name";
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    window.location.href = `mailto:example@example.com?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(body)}`;
+  };
+
   return (
     <nav className="fixed inset-y-0 left-0 flex flex-col justify-between h-full py-">
       <div className="flex flex-col">
-        <Link
-          href={"/"}
+        <a
+          href={"https://www.instagram.com/flowergoods.studio/"}
+          target="_blank"
+          rel="noopener"
           key="instagram"
           className="px-4 py-8 text-gray-700 transform -rotate-90"
         >
           instagram
-        </Link>
-        <Link
-          href={"/"}
+        </a>
+        <a
+          href="#"
+          onClick={handleClick}
           key="email"
           className="px-4 py-8 text-gray-700 transform -rotate-90"
         >
           email
-        </Link>
+        </a>
         <Link
           href={"/About"}
           key="About"
