622b26d63a2abc4af237ab7fb1975971d975f366
Samuel Parker <samuel.a.parker00@gmail.com>
2023-05-12 16:39:31 -0400

    add Layout bottom margin and video loading screen

--- components/Video.tsx
+++ components/Video.tsx
@@ -1,16 +1,27 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 interface Props {
   src: string;
 }
 
-//to do before hosting add: "autoPlay loop" attributes to video element
-
 const Video: FC<Props> = ({ src }) => {
-  console.log("SRC: ", src);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLoadedData = () => {
+    setIsLoading(false);
+  };
+
   return (
     <div className="relative pb-9/16">
-      <video className="" controls muted autoPlay loop>
+      {isLoading && <div className="loading-screen">Video Loading...</div>}
+      <video
+        className={isLoading ? "hidden" : ""}
+        controls
+        muted
+        autoPlay
+        loop
+        onLoadedData={handleLoadedData}
+      >
         <source src={src} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
