fdab407621a8617ac731f09380a7d502cadd53ce
Samuel Parker <samuel.a.parker00@gmail.com>
2023-05-12 16:28:32 -0400

    add screen resize

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,75 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import Video from "../components/Video";
+import { useEffect, useState } from "react";
 import Layout from "../components/shared/Layout";
 import Heading from "../components/Heading";
 import StudioName from "@/components/StudioName";
 import By from "@/components/By";
 import NewYork from "@/components/NewYork";
+import Video from "../components/Video";
 
-import { useEffect } from "react";
+const Home = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
 
-export default function Home() {
   useEffect(() => {
-    document.body.style.backgroundColor = "#64cd85";
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768); // Adjust the breakpoint as needed
+      setIsLoaded(true);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Set initial value
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
+
+  if (!isLoaded) {
+    return null; // Render nothing until the initial loading is complete
+  }
 
   return (
     <Layout pageTitle="Home Page">
-      {/* SMALLER THAN DESKTOP*/}
-      <div className="lg:hidden">
-        <Heading className="my-8 sm:text-7xl text-4xl font-light text-right" />
-        <div>
-          <div className="flex flex-col items-center">
-            <div>
-              <By className="font-thin text-base pb-20" />
-              <StudioName className="text-base font-medium sm:text-2xl" />
+      {isSmallScreen ? (
+        // SMALLER THAN DESKTOP
+        <div className="">
+          <Heading className="my-8 sm:text-7xl text-4xl font-light text-right" />
+          <div>
+            <div className="flex flex-col items-center">
+              <div>
+                <By className="font-thin text-base pb-20" />
+                <StudioName className="text-base font-medium sm:text-2xl" />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="my-4">
-          <Video src="/fg-vid-1.mp4" />
-        </div>
-        <div className="flex flex-col items-center py-4">
-          <NewYork className="text-base font-thin pb-20" />
-        </div>
-      </div>
-
-      {/* DESKTOP >1024px*/}
-      <div className="flex-row hidden lg:flex ">
-        <div>
-          <div className="flex flex-row items-center mt-40 pr-20">
-            <By className="font-thin text-3xl pr-10" />
+          <div className="my-4">
             <Video src="/fg-vid-1.mp4" />
           </div>
-          <StudioName className="pt-20 text-4xl font-bold" />
-        </div>
-        <div className="border-l-[1px] border-black mt-20 pl-20">
-          <Heading className="text-8xl text-center" />
-          <div className="mt-14">
-            <Video src="/fg-vid-2.mp4" />
+          <div className="flex flex-col items-center py-4">
+            <NewYork className="text-base font-thin pb-20" />
           </div>
-          <NewYork className="pt-10 font-light text-2xl text-center" />
         </div>
-      </div>
+      ) : (
+        // DESKTOP >1024px
+        <div className="flex flex-row">
+          <div>
+            <div className="flex flex-row items-center mt-40 pr-20">
+              <By className="font-thin text-3xl pr-10" />
+              <Video src="/fg-vid-1.mp4" />
+            </div>
+            <StudioName className="pt-20 text-4xl font-bold" />
+          </div>
+          <div className="border-l-[1px] border-black mt-20 pl-20">
+            <Heading className="text-8xl text-center" />
+            <div className="mt-14">
+              <Video src="/fg-vid-2.mp4" />
+            </div>
+            <NewYork className="pt-10 font-light text-2xl text-center" />
+          </div>
+        </div>
+      )}
     </Layout>
   );
-}
+};
+
+export default Home;
