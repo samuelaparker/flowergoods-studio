6dc9d2cc23f0069b9c70fdee687791cedbc68a13
Samuel Parker <samuel.a.parker00@gmail.com>
2023-06-22 19:43:28 -0400

    remove loading in video

--- components/Video.tsx
+++ components/Video.tsx
@@ -1,18 +1,22 @@
 import React, { FC, useEffect, useRef, useState } from "react";
+import Loading from "../components/shared/LoadingSpinner";
+
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 interface Props {
   src: string;
 }
 
 const Video: FC<Props> = ({ src }) => {
-  // const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const videoRef = useRef<HTMLVideoElement | null>(null);
 
-  // const handleLoadedData = () => {
-  //   //TEST OF SUSPENSE BOUNDARY
+  const handleLoadedData = () => {
+    //TEST OF SUSPENSE BOUNDARY
 
-  //   setIsLoading(false);
-  // };
+    setIsLoading(false);
+  };
 
   useEffect(() => {
     if (videoRef.current) {
@@ -24,11 +28,15 @@ const Video: FC<Props> = ({ src }) => {
   return (
     <div className="">
       <div className="relative pb-9/16">
-        {/* {isLoading && <div>Video Loading...</div>} */}
+        {/* {isLoading && (
+          <div className="min-h-[300px] flex items-center">
+            <h1 className="">..Loading video</h1>
+          </div>
+        )} */}
         <video
           ref={videoRef}
-          // className={isLoading ? "invisible" : ""}
-          // onLoadedData={handleLoadedData}
+          className={isLoading ? "invisible" : ""}
+          onLoadedData={handleLoadedData}
           loop
           autoPlay
           muted
--- components/shared/loading.js
+++
@@ -1,8 +0,0 @@
-import Skeleton from "react-loading-skeleton";
-
-export default function Loading() {
-  console.log("loading component");
-  // You can add any UI inside Loading, including a Skeleton.
-  return <div className="h-full w-full">LOADING...</div>;
-  //   return <Skeleton />;
-}
