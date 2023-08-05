daf718237933d926a02fbeab2df3470d019a4016
Samuel Parker <samuel.a.parker00@gmail.com>
2023-08-05 15:52:14 -0400

    add meta script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="facebook-domain-verification"
+          content="vmz20p53g8bsodl9kle05xneb53j8t"
+        />
+      </Head>
       <body className="lg:overflow-hidden">
         <Main />
         <NextScript />
