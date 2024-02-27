96c96cef11f96972b53799588daa4b13b859cfd1
Samuel Parker <samuel.a.parker00@gmail.com>
2024-02-27 09:28:11 -0500

    disable hover effect on send flowers for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ const Home = () => {
         </div>
         <div className="container mx-auto flex">
           <div className="m-auto flex flex-col items-center">
-            <div className=" m-auto md:mt-32 mt-12 group font-display text-7xl tracking-wide text-center text-brand-green hover:text-brand-purple transition duration-700 ease-in-out">
+            <div className=" m-auto md:mt-32 mt-12 group font-display text-7xl tracking-wide text-center text-brand-green md:hover:text-brand-purple transition duration-700 ease-in-out">
               <Link
                 href="https://shop.flowergoods.studio/collections/sendflowers"
                 prefetch={true}
