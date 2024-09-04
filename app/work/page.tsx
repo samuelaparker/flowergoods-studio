0e654ae87520843b6aba54c37c516d3fff1ee9a7
Samuel Parker <samuel.a.parker00@gmail.com>
2024-09-04 12:18:17 -0400

    add images to work page

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,5 +1,29 @@
+import Image from "next/image";
+import imageCollageDesktop from "@/public/image-collage-desktop.png";
+import imageCollageMobile from "@/public/image-collage-mobile.png";
+import aGlanceAtOurWork from "@/public/a-glance-at-our-work.png";
 const Work: React.FC = () => {
-  return <></>;
+  return (
+    <>
+      <div className="mt-8 p-3 relative">
+        <div className="absolute lg:w-1/4 w-2/3 left-6 lg:-top-3 -top-5">
+          <Image src={aGlanceAtOurWork} alt="A glance at our work" />
+        </div>
+        <div className="lg:block hidden">
+          <Image
+            src={imageCollageDesktop}
+            alt="collage of past floral arrangements"
+          />
+        </div>
+        <div className="block lg:hidden">
+          <Image
+            src={imageCollageMobile}
+            alt="collage of past floral arrangements"
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Work;
